bca327d63f9ab70925fb8727419856740215b360
moon <moon.kyong@automattic.com>
2023-06-12 20:42:10 -0700

    Use woocommerce-core-profiler for from value

--- plugins/woocommerce-admin/client/core-profiler/index.tsx
+++ plugins/woocommerce-admin/client/core-profiler/index.tsx
@@ -1199,6 +1199,7 @@ export const coreProfilerStateMachineDefinition = createMachine( {
 								redirectUrl: getAdminLink(
 									'admin.php?page=wc-admin'
 								),
+								from: 'woocommerce-core-profiler',
 							} ),
 						onDone: {
 							actions: [
